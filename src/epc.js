ab37b3330137eb96d75c38476f1a20ca86ef2847
Jason Linehan <jlinehan@wikimedia.org>
2019-09-05 10:28:58 -0400

    ENH: Changes scheduling in output buffer; adds comments

--- src/epc.js
+++ src/epc.js
@@ -1,21 +1,47 @@
 /******************************************************************************
- * Wikimedia Foundation event platform client (EPC) prototype 
+ * Event Platform Client (EPC) 
  *
- * Manager 
- * Input
- * Output
- * Sample
- * Token 
- * 
- * @organization: Wikimedia Foundation
- * @author:       Jason Linehan <jlinehan@wikimedia.org>
- * @author:       Mikhail Popov <mpopov@wikimedia.org>
+ * DESCRIPTION 
+ *     Collects events in an input buffer, adds some metadata, places them 
+ *     in an ouput buffer where they are periodically bursted to a remote 
+ *     endpoint via HTTP POST.
+ *
+ *     Designed for use with MediaWiki browser clients producing events to 
+ *     the EventGate intake service.
+ *
+ * LICENSE NOTICE
+ *     Copyright (C) 2019 Wikimedia Foundation 
+ *
+ *     This program is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU General Public License
+ *     as published by the Free Software Foundation; either version 2
+ *     of the License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+ *     02110-1301, USA.
+ *
+ * AUTHORS
+ *     Jason Linehan <jlinehan@wikimedia.org>
+ *     Mikhail Popov <mpopov@wikimedia.org>
+ *
  ******************************************************************************/
-
 
 /******************************************************************************
  * MOCKS 
  ******************************************************************************/
+/**
+ * Mock the stream configuration delivery mechanism. 
+ *
+ * In production, stream configuration JSON will be delivered 
+ * either by a separate HTTP request or a ResourceLoader module. 
+ */
 function MOCK_STREAM_CONFIG()
 {
         return {
@@ -47,26 +73,72 @@ function MOCK_WIKI_URI()
         return "enwiki.myrandomthing.org";
 }
 
+/**
+ * Will be replaced with conditions such as "is DNT enabled" and
+ * other user-defined criteria that will prevent analytics.
+ */
 function MOCK_GLOBAL_IS_COLLECTION_ENABLED();
 {
         return true;
 }
 
 /******************************************************************************
- * HTTPRequestScheduler 
+ * Output 
+ *
+ * The output module buffers events being transmitted to the remote intake 
+ * server. It is especially helpful on mobile clients where it mitigates: 
+ *
+ * - Loss of connection
+ *      Events generated during connection loss still persist in
+ *      the buffer. If the connection is restored, they can still
+ *      be transmitted. 
+ *
+ * - Poor battery life 
+ *      The network request profile is shaped so that requests are
+ *      sent in "bursts", separated by downtime, during which the
+ *      request data dwells in the buffer. 
+ *
+ *      This has been shown to improve battery life by allowing 
+ *      the radio to enter its RRC idle state and other low-power 
+ *      states during the time between bursts. 
  ******************************************************************************/
 var Output = (function() 
 {
-        var BURST_SIZE = 1;
-        var BURST_WAIT = 2000;
+        var WAIT_ITEMS = 1;
+        var WAIT_MS = 2000;
 
-        var BUFFER  = [];
+        var QUEUE   = [];
         var TIMEOUT = null;
         var ENABLED = true;
 
+        /**
+         * NOTE
+         * When the service is re-enabled, there may be items on the
+         * queue, that do not have any timer waiting to send them.
+         * 
+         * Should it be the caller's responsibility to decide whether 
+         * to call send_all_scheduled() or not, after sending has been 
+         * re-enabled?
+         *
+         * We have already decided to expose send_all_scheduled() for
+         * the cases where an application event might require it, e.g.
+         * 'unload'. Should we then delegate the choice to them? It still
+         * exposes details of the timing mechanism. 
+         *
+         * Perhaps we should have it automatically try on enable. It's
+         * just that it's not clear from saying 'enable_sending()' that
+         * sends will actually be happening.
+         *
+         * No, I think it should be done. We shouldn't expose scheduling
+         * details to the caller, and also, send_all_scheduled() implies
+         * that "being in the buffer" is equivalent to "being scheduled";
+         * therefore it should be understood that enabling sending will
+         * also enable th scheduler to begin running once again. 
+         */
         function enable_sending()
         {
                 ENABLED = true;
+                send_all_scheduled();
         }
 
         function disable_sending()
@@ -74,24 +146,87 @@ var Output = (function()
                 ENABLED = false;
         }
 
-        /* Bypass the scheduler, buffer, etc. */
-        function send(url, str)
+        function unschedule()
         {
+                clearTimeout(TIMEOUT);
+        }
+
+        /**
+         * Schedule an item for sending. 
+         *
+         * @url   : The target of the HTTP request 
+         * @str   : The data to send as the POST body
+         * @return: nothing
+         *
+         * NOTE
+         * If sending is not enabled, the scheduler will simply add the
+         * item to the queue and return.
+         *
+         * If sending is enabled, The scheduler will check the queue length. 
+         *      If there are enough items in the queue, it will trigger a burst.
+         *      Otherwise, it will reset the timeout which triggers a burst.
+         */
+        function schedule(url, str) 
+        {
+                QUEUE.append([url, str]);
+
                 if (ENABLED === true) {
-                        navigator.sendBeacon(url, str);
-                } else {
-                        schedule(url, str);
                         /* 
-                         * Option 1: schedule(url, str);
-                         * Option 2: return; the data is silently lost 
+                         * >= because we might have been disabled and 
+                         * accumulated who knows how many without sending.
                          */
+                        if (QUEUE.length >= WAIT_ITEMS) {
+                                send_all_scheduled();
+                        } else {
+                                unschedule();
+                                TIMER = setTimeout(send_all_scheduled, WAIT_MS);
+                        }
                 }
         }
 
+        /**
+         * NOTE
+         * There are five ways to reach this function.
+         *
+         *      1. From schedule():
+         *              An item made QUEUE.length >= BURST_SIZE 
+         *      2. From send():
+         *              We burst early because the radio is now awake
+         *      3. From enable_sending():
+         *              In case timer has expired while disabled, we send here 
+         *      4. From the burst timeout firing:
+         *              Now that events have died down, we start the burst
+         *      5. From another event (e.g. 'unload') firing:
+         *              The handler may call this method to flush the buffer.
+         *
+         * In case 1, 2, and 5, we are bursting "early", so there may be a 
+         * burst timeout counting down, and we need to unschedule() it. 
+         *
+         * In case 3, we may be bursting "early," or we may be bursting "late",
+         * but either way we need to unschedule() the timer. 
+         *
+         * I don't like unschedule() being a side effect of this function, 
+         * but we only have control over cases 1-4. In case 5, the caller
+         * would have to know to call unschedule() and that's just silly. 
+         *
+         * NOTE
+         * Any data in QUEUE will be lost after this function returns. 
+         * There is no possibility to recover from a failed HTTP request.
+         *
+         *      - If connection is lost during a burst, the entire burst 
+         *        will be lost. The event will not fire until after this
+         *        function returns (due to JS). This should be the spec.
+         *
+         *      - If the output is disabled during a burst, the entire
+         *        burst will still be sent. The disablement will not be
+         *        handled until after this function returns (due to JS).
+         */
         function send_all_scheduled()
         {
+                unschedule();
+
                 if (ENABLED === true) {
-                        var items = BUFFER.splice(0, BUFFER.length);
+                        var items = QUEUE.splice(0, QUEUE.length);
                         for (var i=0; i<items.length; i++) {
                                 send(item[i][0], item[i][1]);
                         }
@@ -103,22 +238,38 @@ var Output = (function()
                 }
         }
 
-        function schedule(url, str) 
+        /**
+         * Initiate an asynchronous HTTP POST request.
+         *
+         * @url   : The target of the HTTP request 
+         * @str   : The data to send as the POST body
+         * @return: nothing
+         *
+         * NOTE 
+         * If output is disabled (no HTTP requests are possible), 
+         * the data will be scheduled on to the buffer and will be 
+         * sent when/if output is enabled again. 
+         *
+         * Otherwise the request will be initiated right away, and
+         * the data will never hit the buffer.
+         *
+         * NOTE
+         * Since this request will be waking up the radio, it will
+         * make sense for it to trigger an "unschedule and flush"
+         * operation to take advantage of the fact that we know the
+         * radio is now awake.
+         */
+        function send(url, str)
         {
-                clearTimeout(TIMEOUT);
-
-                BUFFER.append([url, str]);
-
                 if (ENABLED === true) {
+                        navigator.sendBeacon(url, str);
+                        send_all_scheduled();
+                } else {
+                        schedule(url, str);
                         /* 
-                         * >= because we might have been disabled and 
-                         * accumulated who knows how many without sending.
+                         * Option 1: schedule(url, str);
+                         * Option 2: return; the data is silently lost 
                          */
-                        if (BUFFER.length >= BATCH_SIZE) {
-                                send_all_scheduled();
-                        } else {
-                                TIMEOUT = setTimeout(send_all_scheduled, BURST_WAIT);
-                        }
                 }
         }
 
@@ -138,8 +289,9 @@ var Output = (function()
         
         window.addEventListener('online', function() { 
                 enable_sending();
-                send_all_scheduled();
         });
+
+        /* TODO: unload */
 
         return {
                 "schedule": schedule,
@@ -167,7 +319,7 @@ var Output = (function()
  *
  * That's good because also on the apps, certain information like the wiki
  * and stuff that is coupled would need to exist in this module too.
- */
+ ******************************************************************************/
 var Input = (function()
 {
         var QUEUE = [];
