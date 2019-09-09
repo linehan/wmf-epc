bf9becaf531327e4753a64ffaaf17ee510e9c6e7
Jason Linehan <jlinehan@wikimedia.org>
2019-09-09 13:42:39 -0400

    WIP: Java Stream MVP

--- src/java/Stream.java
+++ src/java/Stream.java
@@ -1,36 +1,49 @@
-import org.json.JSONObject
-import java.util.Queue
+import org.json.JSONObject;
+//import java.util.Queue;
 
 class Stream
 {
         JSONObject STREAM;
-        Map<String, String> CASCADE; 
+        //Map<String, String> CASCADE; 
+
+        private String get_stream_config()
+        {
+                return "{edit: {stream: \"edit\",scope: \"session\",sample: 0.06,active: true,url: \"/log\", schema_url:'foo.foo' }}";
+        }
+
+        private String MOCK_WIKI_URI()
+        {
+                return "en";
+        }
+
+        private String MOCK_WIKI_DOMAIN()
+        {
+                return "wikipedia.org";
+        }
 
         public void init()
         {
-                STREAM  = get_stream_config();
-                CASCADE = new HashMap<String, String>();
+                STREAM = new JSONObject(get_stream_config());
+                //CASCADE = new HashMap<String, String>();
         }
 
         private boolean is_stream_enabled(String name) 
         {
-                if (!MOCK_GLOBAL_IS_COLLECTION_ENABLED()) {
+                //if (!MOCK_GLOBAL_IS_COLLECTION_ENABLED()) {
+                        //return false;
+                //}
+
+                if (!STREAM.has(name)) {
+                        /* This name has no configuration */
                         return false;
                 }
 
-                if ("name not in stream") {
-                        return false;
-                }
-
-                if ("active" in STREAM[name] && STREAM[name].active === false) { 
+                JSONObject stream = STREAM.getJSONObject(name);
+                if (stream.has("active") && stream.getBoolean("active") == false) {
                         return false;
                 }
 
                 return true;
-        }
-
-        private boolean is_event_orphaned(String name)
-        {
         }
 
         private boolean is_stream_sampled(String name) 
@@ -38,30 +51,46 @@ class Stream
                 return true;
         }
 
-        public String stream_scope(String name) 
-        {
-        }
-
         public String stream_start(String name)
         {
+                return "";
         }
 
-        public function event(String name, JSONObject data, String timestamp)
+        public void event(String name, JSONObject data, String timestamp)
         {
+                if (!STREAM.has(name)) {
+                        return;
+                }
+
+                JSONObject conf = STREAM.getJSONObject(name);
+                String scope = "unknown";
+                if (conf.has("scope")) {
+                        scope = conf.getString("scope");
+                }
+
                 JSONObject meta = new JSONObject();
-                meta.put("id", Integration.generate_UUID_v4());
+                meta.put("id", "fff");
                 meta.put("dt", timestamp);
                 meta.put("domain", MOCK_WIKI_DOMAIN());
                 meta.put("uri", MOCK_WIKI_URI());
-                meta.put("stream", STREAM[name].stream_name;
+                meta.put("stream", conf.getString("stream"));
 
                 data.put("meta", meta);
 
-                data.put("$schema", STREAM[name].schema_url);
-                data.put("session", Token.session());
-                data.put("pageview", Token.pageview());
-                data.put("activity", Token.activity(name, stream_scope(name)));
+                data.put("$schema",  conf.getString("schema_url"));
+                //data.put("session",  Token.session());
+                //data.put("pageview", Token.pageview());
+                //data.put("activity", Token.activity(name, scope));
 
-                Output.schedule(STREAM[name].url, data.toString());
+                System.out.printf("%s\n", data.toString());
+                //Output.schedule(conf.getString("url"), data.toString());
+        }
+
+        public static void main(String []args)
+        {
+                Stream s = new Stream();
+                s.init();
+
+                s.event("edit", new JSONObject("{msg:'hello, world!'}"), "1997");
         }
 }
